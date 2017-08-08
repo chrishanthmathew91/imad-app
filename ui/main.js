246ec243ad6a793c8127ced4198fdcf3f3dcfc01
chrishanthmathew91 <30715801+chrishanthmathew91@users.noreply.github.com>
2017-08-08 12:24:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,6 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML = 'New text';
+
+var img = document.getElementById('madi');
+element.style.marginLeft = '100px';
