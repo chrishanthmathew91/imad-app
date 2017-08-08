cc4694d0dd9237869bdbfbdda6e77a36a5469a75
chrishanthmathew91 <30715801+chrishanthmathew91@users.noreply.github.com>
2017-08-08 16:32:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,10 +82,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function (req, res) {
-  var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
-});
+// app.get('/:articleName', function (req, res) {
+//   var articleName = req.params.articleName;
+//   res.send(createTemplate(articles[articleName]));
+// });
 
 var counter = 0;
 app.get('/counter', function (req, res) {
