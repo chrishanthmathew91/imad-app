677f14dba54eff4fcbf35f83e2309b5326d20516
chrishanthmathew91 <30715801+chrishanthmathew91@users.noreply.github.com>
2017-08-09 12:42:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,17 @@
 var button = document.getElementById('counter');
-var count = 0;
 button.onclick = function() {
+    var request = new XMLHttpRequest();
     
-    count++;
-    var span = document.getElementById('count');
-    span.innerHTML = count;
+    request.onreadystatechange = function() {
+        if (request.readyState == XMLHttpRequest.DONE) {
+            if (request.state == 200) {
+                var count = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = count;
+            }
+        }
+    };
     
+    request.open('GET', 'http://chrishanthmathew91.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
