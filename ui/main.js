b04ae9ffc46a444e388eef196e54aab0ff0aff5c
chrishanthmathew91 <30715801+chrishanthmathew91@users.noreply.github.com>
2017-08-11 22:44:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,14 +23,14 @@ submit.onclick = function() {
     request.onreadystatechange = function() {
         if (request.readyState == XMLHttpRequest.DONE) {
             if (request.status == 200) {
-                var names = request.responseText;
-                names = JSON.parse(names);
+                var jsonString = request.responseText;
+                var objArray = JSON.parse(jsonString);
                 var list = '';
-                for (var i=0; i<names.length; i++) {
-                    list += '<li>' + names[i] + '</li>'
+                for (var i=0; i<objArray.length; i++) {
+                    list += '<div><blockquote class="blockquote-reverse"><p>' + objArray[i].comment + '</p><footer>' + objArray[i].name + '</footer></blockquote></div>'
                 }
-                var nameList = document.getElementById('name_list');
-                nameList.innerHTML = list;
+                var commentList = document.getElementById('comment_list');
+                commentList.innerHTML = list;
             }
         }
     };
