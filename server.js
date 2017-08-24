7e87b27c15018df4e641177ebd20636acb8ebc28
chrishanthmathew91 <30715801+chrishanthmathew91@users.noreply.github.com>
2017-08-24 16:16:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+const { Pool, Client } = require('pg');
+
+var config = {
+  user: 'chrishanthmathew91',
+  host: 'db.imad.hasura-app.io',
+  database: 'chrishanthmathew91',
+  port: 5432,
+  password: process.env.DB_PASSWORD,
+}
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -80,6 +90,17 @@ function createTemplate (data) {
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function(req, res) {
+    pool.query('SELECT * FROM test', function(err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }
+    });
 });
 
 var counter = 0;
