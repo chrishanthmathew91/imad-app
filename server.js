22fa62835c07d2e7bf2cf76651a5239534a40ea8
chrishanthmathew91 <30715801+chrishanthmathew91@users.noreply.github.com>
2017-08-24 16:26:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
-const { Pool, Client } = require('pg');
+const { Pool } = require('pg')
 
 var config = {
   user: 'chrishanthmathew91',
