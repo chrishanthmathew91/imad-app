a91b6b7e1e8e80b970ebc9697b43c13689dc7820
chrishanthmathew91 <30715801+chrishanthmathew91@users.noreply.github.com>
2017-09-03 21:30:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var crypto = required('crypto');
 
 const Pool = require('pg').Pool;
 
@@ -54,8 +55,18 @@ function createTemplate (data) {
 
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+function hash (input, salt) {
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input', function(req, res) {
+    var hashedString = hash(req.params.input, 'this-is-some-random-string');
+    res.send(hashedString);
+})
 
 var pool = new Pool(config);
 app.get('/test-db', function(req, res) {
