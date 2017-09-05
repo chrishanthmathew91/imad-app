47e6956098d339d849e25b00f25aaa7a40595731
chrishanthmathew91 <30715801+chrishanthmathew91@users.noreply.github.com>
2017-09-05 19:01:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,26 @@
-var button = document.getElementById('counter');
+var button = document.getElementById('login_btn');
 button.onclick = function() {
     var request = new XMLHttpRequest();
     
     request.onreadystatechange = function() {
-        if (request.readyState == XMLHttpRequest.DONE) {
-            if (request.status == 200) {
-                var count = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = count;
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                console.log('User logged in');
+                alert('Logged in successfully');
+            } else if (request.stauts === 403) {
+                alert('username/password is invalid');
+            } else if (request.status === 500) {
+                alert('Something went wrong on the server');
             }
         }
     };
-    
-    request.open('GET', 'http://chrishanthmathew91.imad.hasura-app.io/counter', true);
-    request.send(null);
+    var username = document.getElementById('username');
+    var password = document.getElementById('password');
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://chrishanthmathew91.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
 };
 
 var submit = document.getElementById('submit_btn');
